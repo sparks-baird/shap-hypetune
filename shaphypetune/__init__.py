19b506a187674bb0de851c7d0d34ac41d0d38fc2
sgbaird <sterling.baird@icloud.com>
2022-01-14 20:04:24 -0700

    Update __init__.py

--- shaphypetune/__init__.py
+++ shaphypetune/__init__.py
@@ -1,3 +1,6 @@
+"""A python package for simultaneous Hyperparameters Tuning and Features Selection for
+Gradient Boosting Models."""
+__version__ = "0.2.4"
 from .utils import *
 from ._classes import *
 from .shaphypetune import *
